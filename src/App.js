2400277705b7ec2ebe4de0d09646b833c38d3180
Dylan <szalank.dj@gmail.com>
2023-05-07 13:29:38 -0400

    recreated all directories and files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+
+const App = () => {
+
+    return(
+        <h1>Hello World</h1>
+    );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./App";
 import { BrowserRouter } from "react-router-dom";
-import "./style/index.css";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
