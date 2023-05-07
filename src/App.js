7ad4b506d668a79fb384c286d94ef490e3d70798
Dylan <szalank.dj@gmail.com>
2023-05-07 16:15:42 -0400

    started react from scratch and started creating the estimate page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,152 @@
-import React, { useState, useEffect } from "react";
-import { Route, Routes } from "react-router-dom";
 
-const App = () => {
+import './App.css';
 
-    return(
-        <h1>Hello World</h1>
-    );
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <div className='header'>
+        <div className='app-title'>JobTrax</div>
+
+        <div className='nav-links'>
+          <div className='links'>Estimate</div>
+          <div className='links'>Accounting</div>
+          <div className='links'>Project Management</div>
+          <div className='links'>CRM</div>
+          <div className='links'>Login/Register</div>
+          
+        </div>
+      </div>
+
+      <div className='estimate'>
+        <div className='Logo'></div>
+        <div className='project-running-totals-box'>
+          <h3>Project Running Totals</h3>
+          <div className='totals-sub-box'>
+            <div className='sub-boxes'>
+              <h3>Base Cost</h3>
+              <p>$$$</p>
+            </div>
+            <div className='sub-boxes'>
+              <h3>Profit Amount</h3>
+              <p>$$$</p>
+            </div>
+            <div className='sub-boxes'>
+              <h3>Margin</h3>
+              <p>$$$</p>
+            </div>
+            <div className='sub-boxes'>
+              <h3>Total Cost</h3>
+              <p>$$$</p>
+            </div>
+          </div>
+        </div>
+
+        <form className='estimate-form'>
+          <div className='category'>
+            <label>Category</label>
+            <select name='category' className='category'>
+              <option value="Building Permit">General Conditions</option>
+              <option value="Site Work">Site Work</option>
+              <option value="General Conditions">Concrete</option>
+              <option value="Carpentry">Carpentry</option>
+            </select>
+            </div> 
+          <div className='subcategory'>
+            <label>Subcategory</label>
+            <select name='subcategory' className='subcategory'>
+              <option value="Building Permit">Building Permit</option>
+              <option value="Site Work">Site Work</option>
+              <option value="General Conditions">General Conditions</option>
+              <option value="Carpentry">Carpentry</option>
+            </select>
+            </div> 
+          <div className='category'>
+            <label>Category</label>
+            <select name='category' className='category'>
+              <option value="Building Permit">Building Permit</option>
+              <option value="Site Work">Site Work</option>
+              <option value="General Conditions">General Conditions</option>
+              <option value="Carpentry">Carpentry</option>
+            </select>
+            </div> 
+          <div className='category'>
+            <label>Category</label>
+            <select name='category' className='category'>
+              <option value="Building Permit">Building Permit</option>
+              <option value="Site Work">Site Work</option>
+              <option value="General Conditions">General Conditions</option>
+              <option value="Carpentry">Carpentry</option>
+            </select>
+            </div> 
+          <div className='category'>
+            <label>Category</label>
+            <select name='category' className='category'>
+              <option value="Building Permit">Building Permit</option>
+              <option value="Site Work">Site Work</option>
+              <option value="General Conditions">General Conditions</option>
+              <option value="Carpentry">Carpentry</option>
+            </select>
+            </div> 
+          <div className='category'>
+            <label>Category</label>
+            <select name='category' className='category'>
+              <option value="Building Permit">Building Permit</option>
+              <option value="Site Work">Site Work</option>
+              <option value="General Conditions">General Conditions</option>
+              <option value="Carpentry">Carpentry</option>
+            </select>
+            </div> 
+          <div className='category'>
+            <label>Category</label>
+            <select name='category' className='category'>
+              <option value="Building Permit">Building Permit</option>
+              <option value="Site Work">Site Work</option>
+              <option value="General Conditions">General Conditions</option>
+              <option value="Carpentry">Carpentry</option>
+            </select>
+            </div> 
+          <div className='category'>
+            <label>Category</label>
+            <select name='category' className='category'>
+              <option value="Building Permit">Building Permit</option>
+              <option value="Site Work">Site Work</option>
+              <option value="General Conditions">General Conditions</option>
+              <option value="Carpentry">Carpentry</option>
+            </select>
+            </div> 
+          <div className='category'>
+            <label>Category</label>
+            <select name='category' className='category'>
+              <option value="Building Permit">Building Permit</option>
+              <option value="Site Work">Site Work</option>
+              <option value="General Conditions">General Conditions</option>
+              <option value="Carpentry">Carpentry</option>
+            </select>
+            </div> 
+          <div className='category'>
+            <label>Category</label>
+            <select name='category' className='category'>
+              <option value="Building Permit">Building Permit</option>
+              <option value="Site Work">Site Work</option>
+              <option value="General Conditions">General Conditions</option>
+              <option value="Carpentry">Carpentry</option>
+            </select>
+            </div> 
+          <div className='category'>
+            <label>Category</label>
+            <select name='category' className='category'>
+              <option value="Building Permit">Building Permit</option>
+              <option value="Site Work">Site Work</option>
+              <option value="General Conditions">General Conditions</option>
+              <option value="Carpentry">Carpentry</option>
+            </select>
+            </div> 
+
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default App;
