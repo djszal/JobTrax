e8608a1757ae84ac5acd9155c0eb232244892fa0
Dylan <szalank.dj@gmail.com>
2023-05-10 16:26:49 -0400

    added image to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,10 @@ import React from "react";
 
 const Home = () => {
     return(
+        <>
         <h1>Hellow World</h1>
+        <img src="https://www.bankrate.com/2020/09/17105340/Home_construction_loans_explained.jpg"></img>
+        </>
     )
 }
 
